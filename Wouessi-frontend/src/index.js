3eb633fc893fd3c06cc5c83e9545380e64fc7071
Juny7736 <handaniel721@gmail.com>
2025-03-27 14:04:57 -0400

    restored it the app

--- Wouessi-frontend/src/index.js
+++ Wouessi-frontend/src/index.js
@@ -2,11 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
-import InternshipCertificate from "./pages/InternshipCertificate/InternshipCertificate";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <InternshipCertificate></InternshipCertificate>
+    <App></App>
   </React.StrictMode>
 );
